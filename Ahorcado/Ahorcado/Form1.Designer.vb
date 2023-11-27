62e4d99f45817f1db586df481334b73d6a4f0eab
Ader02 <152221536+Ader02@users.noreply.github.com>
2023-11-27 18:28:57 -0300

    Ahorcado
    Trabajo practico

---
+++ Ahorcado/Ahorcado/Form1.Designer.vb
@@ -0,0 +1,101 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtLetra = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblPalabra = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(91, 57)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Ingrese una letra:"
+        '
+        'txtLetra
+        '
+        Me.txtLetra.Location = New System.Drawing.Point(247, 60)
+        Me.txtLetra.Name = "txtLetra"
+        Me.txtLetra.Size = New System.Drawing.Size(116, 20)
+        Me.txtLetra.TabIndex = 1
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnIngresar.Location = New System.Drawing.Point(171, 104)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(123, 34)
+        Me.btnIngresar.TabIndex = 2
+        Me.btnIngresar.Text = "Ingresar letra"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(92, 177)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(143, 21)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Palabra a Adivinar:"
+        '
+        'lblPalabra
+        '
+        Me.lblPalabra.AutoSize = True
+        Me.lblPalabra.Font = New System.Drawing.Font("Roboto", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPalabra.Location = New System.Drawing.Point(241, 177)
+        Me.lblPalabra.Name = "lblPalabra"
+        Me.lblPalabra.Size = New System.Drawing.Size(14, 21)
+        Me.lblPalabra.TabIndex = 4
+        Me.lblPalabra.Text = "-"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(449, 253)
+        Me.Controls.Add(Me.lblPalabra)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnIngresar)
+        Me.Controls.Add(Me.txtLetra)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtLetra As TextBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPalabra As Label
+End Class
---
+++ Ahorcado/Ahorcado/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim palabras() As String = {"Spiderman", "Hulk", "Batman", "HawkEye", "Iron Man"}
+    Dim palabraSeleccionada As String
+    Dim PalabraAMostrar As String = ""
+    Dim Terminado As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim generador As New Random()
+        palabraSeleccionada = palabras(generador.Next(0, palabras.Length))
+
+        For i As Integer = 0 To palabraSeleccionada.Length - 1
+            If (palabraSeleccionada(i) = " ") Then
+                PalabraAMostrar += " "
+            Else
+                PalabraAMostrar += "-"
+            End If
+        Next
+        lblPalabra.Text = PalabraAMostrar
+    End Sub
+
+    Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        Dim PalabraConLetraAcertada As String = ""
+        Dim LetraActual As String = txtLetra.Text
+
+        For i As Integer = 0 To palabraSeleccionada.Length - 1
+            If (LetraActual = palabraSeleccionada(i)) Then
+                PalabraConLetraAcertada += LetraActual
+            Else
+                PalabraConLetraAcertada += PalabraAMostrar(i)
+            End If
+        Next
+        PalabraAMostrar = PalabraConLetraAcertada
+        lblPalabra.Text = PalabraAMostrar
+
+        If Not PalabraAMostrar.Contains("-") Then
+            Terminado = True
+            MsgBox("Ganaste!!!!")
+            txtLetra.Enabled = False
+            btnIngresar.Enabled = False
+        End If
+    End Sub
+End Class
